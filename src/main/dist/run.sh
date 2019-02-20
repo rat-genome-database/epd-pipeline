ade98f25b9e9b27a1559a4d5c60be939e4fdd1aa
Marek Tutaj <mtutaj@mcw.edu>
2019-02-20 11:00:12 -0600

    updated dependencies

--- src/main/dist/run.sh
+++ src/main/dist/run.sh
@@ -6,8 +6,7 @@ APPNAME=EPD
 
 APPDIR=/home/rgddata/pipelines/$APPNAME
 cd $APPDIR
-pwd
-DB_OPTS="-Dspring.config=$APPDIR/../properties/default_db.xml"
-LOG4J_OPTS="-Dlog4j.configuration=file://$APPDIR/properties/log4j.properties"
-declare -x "${APPNAME}_OPTS=$DB_OPTS $LOG4J_OPTS"
-bin/$APPNAME "$@"
+
+java -Dspring.config=$APPDIR/../properties/default_db2.xml \
+    -Dlog4j.configuration=file://$APPDIR/properties/log4j.properties \
+    -jar lib/$APPNAME.jar "$@"
